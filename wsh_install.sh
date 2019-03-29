cf8f8670629d272d692ef05e95bb1c81b7aa9cf4
kevin olson <acidjazz@gmail.com>
2019-03-29 09:30:59 -0500

    Create wsh_install.sh

---
+++ wsh_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install zsh git ssh
+
+
+chsh -s /usr/bin/zshw
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
