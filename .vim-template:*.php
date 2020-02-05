90039b19a7cbf2821d8f80186f052d0f32546406
kevin olson <acidjazz@gmail.com>
2020-02-05 16:18:50 -0600

    🚧 template love

---
+++ .vim-template:*.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * description for %FFILE%
+ *
+ * @author kevin olson <ko@kittyhawk.io>
+ * @license MIT
+ */
+
+class %FILE% {
+
+  /**
+   * Description for property;
+   *
+   * @var type
+   */
+  protected $property;
+
+
+  /**
+   * Method description
+   * 
+   * @param type $variable
+   *
+   * @throws Exception
+   * @return void
+   */
+  public function method()
+  {
+
+  }
+
+
+}
